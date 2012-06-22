cf8761a61d7aa5a056d8a3a1e8c0b914e253c0da
Yuri Baburov <buriy@mindmakers.ru>
2012-06-22 07:03:12 +0000

    Better fixes for cross-platform compilation. Checked it works in Linux.

--- stdafx.h
+++ stdafx.h
@@ -3,7 +3,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
-#if !defined(WIN32) && !defined(MS_WINDOWS)
+#if defined(WIN32) || defined(MS_WINDOWS)
 #include <tchar.h>
 #endif
 #include <assert.h>
